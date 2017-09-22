165d5d818f88f3e867127b2e32de283e2ecdab8d
Asaf Yatskan <asafyat@gmail.com>
2017-09-22 12:39:36 +0300

    elads commits

--- shell.c
+++ shell.c
@@ -16,6 +16,8 @@ static struct termios old, new;
 /* Initialize new terminal i/o settings */
 void initTermios(int echo) 
 {
+  //Elad comment 
+  printf("hello world\n");
   tcgetattr(0, &old); /* grab old terminal i/o settings */
   new = old; /* make new settings same as old settings */
   new.c_lflag &= ~ICANON; /* disable buffered i/o */
